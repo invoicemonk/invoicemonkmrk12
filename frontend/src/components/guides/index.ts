0087e591ada3aa956887dffab9886aad051f6fcb
Olayinka Olayokun <13032616+Olayokunyinka@users.noreply.github.com>
2026-02-13 09:47:53 +0100

    updates from 15

--- frontend/src/components/guides/index.ts
+++
@@ -1,6 +0,0 @@
-export { GuideHero } from './GuideHero';
-export { ConceptGrid } from './ConceptGrid';
-export { ArticleList } from './ArticleList';
-export { GuideCTA } from './GuideCTA';
-export { GuideFAQ } from './GuideFAQ';
-export { GuideCrossLink } from './GuideCrossLink';
--- nextjs-marketing/hooks/useLocale.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-import { LocaleContext } from '@/contexts/LocaleContext';
-
-export function useLocale() {
-  const context = useContext(LocaleContext);
-  
-  // Context now has a default value, so it's always defined
-  return context;
-}
